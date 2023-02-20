372f13f0ec0926c8ee7f74e54da97ea21d3396dc
Moammad Farhad <mfarhadattari>
2023-02-20 10:52:22 +0600

    rectangle area

--- areaOfCircle.py
+++ areaOfCircle.py
@@ -6,4 +6,4 @@ radius = float(input('Radius of Circle: '))
 pi = math.pi
 
 area = pi * pow(radius , 2)
-print(area)
+print('Area of Circle = ', area)
---
+++ areaOfRectangle.py
@@ -0,0 +1,9 @@
+# area of rectangle
+# area = width * length 
+
+width = float(input('Width of Rectangle: '))
+length = float(input('Length of Rectangle: '))
+
+area = width * length ;
+
+print('Area of Rectangle = ', area) 
