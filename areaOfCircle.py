6209c3541064dc032c307663bb6d76ea70c46e0c
Moammad Farhad <mfarhadattari>
2023-02-20 10:45:45 +0600

    area of circle

---
+++ areaOfCircle.py
@@ -0,0 +1,9 @@
+# area of circle
+# area = pi * (r)**2
+import math
+
+radius = float(input('Radius of Circle: '))
+pi = math.pi
+
+area = pi * pow(radius , 2)
+print(area)
--- areaOfIsosceles.py
+++ areaOfIsosceles.py
@@ -1,6 +1,6 @@
 # Area of isosceles
 # s = (a+b+c) / 2
-# area = s(s-a)(s-b)(s-c)
+# area = (s(s-a)(s-b)(s-c)) ** 0.5
 
 a = float(input('a = ')) 
 b = float(input('b = ')) 
@@ -8,7 +8,7 @@ c = float(input('c = '))
 
 if (a+b > c) and (a+c> b) and (b+c> a) :
     s = (a + b + c) / 2 
-    area = s*(s-a)*(s-b)*(s-c) 
+    area = (s*(s-a)*(s-b)*(s-c) ) ** 0.5
     print('Area of isosceles: ', area)
 else:
     print('Triangle is not possible. ')
